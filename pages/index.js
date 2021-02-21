2219054917d4b9b1170df6874f038c25fdb1790a
marcelokuhn <marcelokuhn@gmail.com>
2021-02-21 18:39:20 -0300

    Contador

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,29 @@
-function Home()
+import { useState } from "react";
+
+function Home() {
+    return (
+        <div>
+            <h1>Home</h1>
+            <Contador/>
+        </div>
+    )
+}
+
+function Contador()
 {
-    return
-    { 
-        <div>Home</div>
+    const [contador, setContador] = useState(1);
+
+    function AddContador()
+    {
+        setContador(contador + 1);
     }
+
+    return (
+        <div>
+            <div>(contador)</div>
+            <button onClick={AddContador}>Adiciona</button>
+        </div>
+    )
 }
 
 export default Home
